af6e4d66b90c52d2f72687381559f3cd7aab46bf
ywang880 <ywang880@yahoo.ca>
2020-12-09 19:48:53 -0500

    add query example

--- SampleAppMongo/src/main/java/com/learning/java/app/App.java
+++ SampleAppMongo/src/main/java/com/learning/java/app/App.java
@@ -1,9 +1,12 @@
 package com.learning.java.app;
 
+import java.util.function.Consumer;
+
 import org.bson.Document;
 
 import com.google.gson.Gson;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -21,14 +24,26 @@ public class App {
 
 			System.out.println("Collection created successfully");
 
-			Customer c = new Customer("john", "Smith", 22, "Engineer", "john@gmail.com", "416-666-555");
-
 			Gson gson = new Gson();
+
+			Customer c = new Customer("john", "Smith", 22, "Engineer", "john@gmail.com", "416-666-555");
 			String json = gson.toJson(c);
 			Document doc = Document.parse(json.toString());
 
 			// insert(dbObject);
 			coll.insertOne(doc);
+
+			c = new Customer("mike", "Smith", 32, "Engineer", "mike@gmail.com", "416-666-111");
+			json = gson.toJson(c);
+			doc = Document.parse(json.toString());
+
+			// insert(dbObject);
+			coll.insertOne(doc);
+
+			// find all documents and print out
+			FindIterable<Document> it = coll.find();
+			Consumer<Document> docConsumer = dc -> System.out.println(dc.toJson());
+			it.forEach(docConsumer);
 
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
